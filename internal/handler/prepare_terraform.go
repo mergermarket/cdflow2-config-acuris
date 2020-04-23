99d8055afefe54788f7488a1aca018e511edca43
osh <karol.slomczynski@gmail.com>
2020-04-23 11:24:21 +0100

    Get AWS secrets for terraform backend config.
    Co-authored-by: Tom <tom.yandell@acuris.com>

--- internal/handler/prepare_terraform.go
+++ internal/handler/prepare_terraform.go
@@ -1,33 +1,34 @@
 package handler
 
 import (
+	"fmt"
 	common "github.com/mergermarket/cdflow2-config-common"
 )
 
 // PrepareTerraform runs before terraform to configure.
 func (h *Handler) PrepareTerraform(request *common.PrepareTerraformRequest, response *common.PrepareTerraformResponse) error {
 
-	// if err := h.InitReleaseAccountCredentials(request.Env, request.Team); err != nil {
-	// 	response.Success = false
-	// 	fmt.Fprintln(h.ErrorStream, err)
-	// 	return nil
-	// }
+	if err := h.InitReleaseAccountCredentials(request.Env, request.Team); err != nil {
+		response.Success = false
+		fmt.Fprintln(h.ErrorStream, err)
+		return nil
+	}
 
-	// releaseAccountCredentialsValue, err := h.ReleaseAccountCredentials.Get()
-	// if err != nil {
-	// 	response.Success = false
-	// 	fmt.Fprintln(h.ErrorStream, err)
-	// 	return nil
-	// }
+	releaseAccountCredentialsValue, err := h.ReleaseAccountCredentials.Get()
+	if err != nil {
+		response.Success = false
+		fmt.Fprintln(h.ErrorStream, err)
+		return nil
+	}
 
-	// // response.TerraformBackendType = "s3"
-	// // response.TerraformBackendConfig["bucket"] = ""
-	// // response.TerraformBackendConfig["region"] = ""
-	// // response.TerraformBackendConfig["key"] = fmt.Sprintf("%s/%s/%s/terraform.tfstate")
-	// //response.TerraformBackendConfig["dynamodb_table"] = ""
-	// response.TerraformBackendConfig["access_key"] = releaseAccountCredentialsValue.AccessKeyID
-	// response.TerraformBackendConfig["secret_key"] = releaseAccountCredentialsValue.SecretAccessKey
-	// response.TerraformBackendConfig["token"] = releaseAccountCredentialsValue.SessionToken
+	response.TerraformBackendConfig["access_key"] = releaseAccountCredentialsValue.AccessKeyID
+	response.TerraformBackendConfig["secret_key"] = releaseAccountCredentialsValue.SecretAccessKey
+	response.TerraformBackendConfig["token"] = releaseAccountCredentialsValue.SessionToken
+	// response.TerraformBackendType = "s3"
+	// response.TerraformBackendConfig["bucket"] = ""
+	// response.TerraformBackendConfig["region"] = ""
+	// response.TerraformBackendConfig["key"] = fmt.Sprintf("%s/%s/%s/terraform.tfstate")
+	// response.TerraformBackendConfig["dynamodb_table"] = ""
 
 	return nil
 }
--- internal/handler/prepare_terraform_test.go
+++ internal/handler/prepare_terraform_test.go
@@ -1,27 +1,40 @@
 package handler_test
 
 import (
+	"github.com/aws/aws-sdk-go/aws/client"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/mergermarket/cdflow2-config-acuris/internal/handler"
+	common "github.com/mergermarket/cdflow2-config-common"
 	"testing"
 )
 
 func TestPrepareTerraform(t *testing.T) {
-	/*
-		// Given
-		request := common.CreatePrepareTerraformRequest()
-		response := common.CreatePrepareTerraformResponse()
+	// Given
+	accessKeyID := "foo"
+	secretAccessKey := "bar"
+	sessionToken := "baz"
+	request := common.CreatePrepareTerraformRequest()
+	request.Env["AWS_ACCESS_KEY_ID"] = "root foo"
+	request.Env["AWS_SECRET_ACCESS_KEY"] = "root bar"
+	response := common.CreatePrepareTerraformResponse()
 
-		mockAssumeRoleProviderFactory := func(session client.ConfigProvider, roleARN, roleSessionName string) credentials.Provider {
-			return createMockAssumeRoleProvider("foo", "bar", "baz")
-		}
-		h := handler.New().
-			WithAssumeRoleProviderFactory(mockAssumeRoleProviderFactory).
-			WithS3ClientFactory(func(client.ConfigProvider) s3iface.S3API {
-				return mockS3Client
-			})
+	mockAssumeRoleProviderFactory := func(session client.ConfigProvider, roleARN, roleSessionName string) credentials.Provider {
+		return createMockAssumeRoleProvider(accessKeyID, secretAccessKey, sessionToken)
+	}
+	h := handler.New().
+		WithAssumeRoleProviderFactory(mockAssumeRoleProviderFactory)
 
-		// When
-		h.PrepareTerraform(request, response)
+	// When
+	h.PrepareTerraform(request, response)
 
-		// Then
-	*/
+	// Then
+	if response.TerraformBackendConfig["access_key"] != accessKeyID {
+		t.Fatalf("Want %q, got %q", accessKeyID, response.TerraformBackendConfig["access_key"])
+	}
+	if response.TerraformBackendConfig["secret_key"] != secretAccessKey {
+		t.Fatalf("Want %q, got %q", secretAccessKey, response.TerraformBackendConfig["secret_key"])
+	}
+	if response.TerraformBackendConfig["token"] != sessionToken {
+		t.Fatalf("Want %q, got %q", sessionToken, response.TerraformBackendConfig["token"])
+	}
 }
