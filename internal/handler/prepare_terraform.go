ebad54b6ba0aaddced19e197e139b44559f204c4
Tom Yandell <tom@tomyandell.co>
2020-04-23 11:03:05 +0100

    WIP configure terraform

--- internal/handler/prepare_terraform.go
+++ internal/handler/prepare_terraform.go
@@ -6,32 +6,28 @@ import (
 
 // PrepareTerraform runs before terraform to configure.
 func (h *Handler) PrepareTerraform(request *common.PrepareTerraformRequest, response *common.PrepareTerraformResponse) error {
-	/*	if err := h.InitReleaseAccountCredentials(request.Env, request.Team); err != nil {
-			response.Success = false
-			fmt.Fprintln(h.ErrorStream, err)
-			return nil
-		}
 
-			session, err := h.createReleaseAccountSession()
-			if err != nil {
-				return fmt.Errorf("unable to create AWS session in release account: %v", err)
-			}
+	// if err := h.InitReleaseAccountCredentials(request.Env, request.Team); err != nil {
+	// 	response.Success = false
+	// 	fmt.Fprintln(h.ErrorStream, err)
+	// 	return nil
+	// }
 
-			releaseAccountCredentialsValue, err := h.ReleaseAccountCredentials.Get()
-			if err != nil {
-				response.Success = false
-				fmt.Fprintln(h.ErrorStream, err)
-				return nil
-			}
+	// releaseAccountCredentialsValue, err := h.ReleaseAccountCredentials.Get()
+	// if err != nil {
+	// 	response.Success = false
+	// 	fmt.Fprintln(h.ErrorStream, err)
+	// 	return nil
+	// }
 
-				response.TerraformBackendType = "s3"
-				response.TerraformBackendConfig["bucket"] = ""
-				response.TerraformBackendConfig["region"] = ""
-				response.TerraformBackendConfig["key"] = fmt.Sprintf("%s/%s/%s/terraform.tfstate")
-				//response.TerraformBackendConfig["dynamodb_table"] = ""
-				response.TerraformBackendConfig["access_key"] = releaseAccountCredentialsValue.AccessKeyID
-				response.TerraformBackendConfig["secret_key"] = releaseAccountCredentialsValue.SecretAccessKey
-				response.TerraformBackendConfig["token"] = releaseAccountCredentialsValue.SessionToken
-	*/
+	// // response.TerraformBackendType = "s3"
+	// // response.TerraformBackendConfig["bucket"] = ""
+	// // response.TerraformBackendConfig["region"] = ""
+	// // response.TerraformBackendConfig["key"] = fmt.Sprintf("%s/%s/%s/terraform.tfstate")
+	// //response.TerraformBackendConfig["dynamodb_table"] = ""
+	// response.TerraformBackendConfig["access_key"] = releaseAccountCredentialsValue.AccessKeyID
+	// response.TerraformBackendConfig["secret_key"] = releaseAccountCredentialsValue.SecretAccessKey
+	// response.TerraformBackendConfig["token"] = releaseAccountCredentialsValue.SessionToken
+
 	return nil
 }
