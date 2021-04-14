4c950f380e825aab55e1f956b523b03749c6b0c7
Marcus Clairmont <47252337+Kintaro-Oe@users.noreply.github.com>
2021-04-14 16:46:32 +0100

    add validateState (#7)

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/mergermarket/cdflow2-config-acuris
 go 1.13
 
 require (
-	github.com/aws/aws-sdk-go v1.33.5
-	github.com/mergermarket/cdflow2-config-common v0.43.6
-	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	github.com/aws/aws-sdk-go v1.38.13
+	github.com/mergermarket/cdflow2-config-common v0.44.1
 )
--- internal/handler/helpers_test.go
+++ internal/handler/helpers_test.go
@@ -1,7 +1,9 @@
 package handler_test
 
 import (
+	"errors"
 	"io"
+	"path"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -35,9 +37,11 @@ func createMockAssumeRoleProvider(accessKeyID, secretAccessKey, sessionToken str
 
 type MockS3Client struct {
 	s3iface.S3API
+	files                  map[string][]byte
 	putObjectCalls         []*s3.PutObjectInput
 	getObjectBody          io.ReadCloser
 	getObjectContentLength int64
+	headObjectMetadata     map[string]*string
 }
 
 func (m *MockS3Client) PutObject(input *s3.PutObjectInput) (*s3.PutObjectOutput, error) {
@@ -49,5 +53,22 @@ func (m *MockS3Client) GetObject(input *s3.GetObjectInput) (*s3.GetObjectOutput,
 	return &s3.GetObjectOutput{
 		Body:          m.getObjectBody,
 		ContentLength: aws.Int64(m.getObjectContentLength),
+	}, nil
+}
+
+func (m *MockS3Client) HeadObject(input *s3.HeadObjectInput) (*s3.HeadObjectOutput, error) {
+	// m.mu.Lock()
+	// defer m.mu.Unlock()
+
+	key := path.Join(*input.Bucket, *input.Key)
+	if _, ok := m.files[key]; !ok {
+		return nil, errors.New("- Key does not exist")
+	}
+
+	// return &s3.HeadObjectOutput{
+	// 	Body: ioutil.NopCloser(bytes.NewReader(m.files[key])),
+	// }, nil
+	return &s3.HeadObjectOutput{
+		Metadata: m.headObjectMetadata,
 	}, nil
 }
